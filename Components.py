553d8ff26ba2214aa370c8f39028e112e4e062ea
shitwolfymakes <bhawk11@gmail.com>
2019-02-25 15:35:35 -0600

    Components.py added to hold complex mission component structures

---
+++ Components.py
@@ -0,0 +1,44 @@
+''' Components.py
+    This file contains the classes defining some components of a mission
+'''
+
+
+class Cargo(object):
+    '''
+    cargo  = [ [None, None, [None, None]], # cargo (random | <name>) <number> [<number> [<probability>]]
+               [None, None, None],         #     illegal <fine> [<message>]
+               None ]                      #     stealth
+    '''
+
+    def __init__(self):
+        print("component %s initialized", self.__class__)
+    #end init
+
+#end class Cargo
+
+
+class Passengers(object):
+
+    def __init__(self):
+        print("component %s initialized", self.__class__)
+    # end init
+
+# end class
+
+
+class Clearance(object):
+
+    def __init__(self):
+        print("component %s initialized", self.__class__)
+    # end init
+
+# end class
+
+
+class Stopover(object):
+
+    def __init__(self):
+        print("component %s initialized", self.__class__)
+    # end init
+
+# end class
