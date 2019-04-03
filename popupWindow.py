3d3a072c7498bacaa67b4b122cf444f8298efedf
shitwolfymakes <bhawk11@gmail.com>
2019-04-03 15:33:22 -0500

    new mission popup now properly prevents gui input to rest of the application

--- popupWindow.py
+++ popupWindow.py
@@ -3,12 +3,14 @@ from tkinter import *
 class popupWindow(object):
     def __init__(self, app, master, text):
         self.app = app
-        top = self.top = Toplevel(master)
-        self.l = Label(top, text=text, bg='white')
+        self.top = Toplevel(master)
+        self.top.title("New Mission")
+        self.top.grab_set()             # freezes the app until the user enters or cancels
+        self.l = Label(self.top, text=text, bg='white')
         self.l.pack()
-        self.e = Entry(top)
+        self.e = Entry(self.top)
         self.e.pack()
-        self.b = Button(top, text='Ok', command=self.cleanup)
+        self.b = Button(self.top, text='Ok', command=self.cleanup)
         self.b.pack()
     #end init
 
@@ -16,8 +18,8 @@ class popupWindow(object):
     def cleanup(self):
         value = self.e.get()
         self.app.addMission(value)
+        self.top.grab_release()         # HAVE TO RELEASE
         self.top.destroy()
-
     #end cleanup
 
 #end class popupWindow()
