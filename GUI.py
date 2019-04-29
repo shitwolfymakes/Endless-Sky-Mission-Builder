977521386eeb931a765015b4f5f9135e34b0300c
shitwolfymakes <bhawk11@gmail.com>
2019-04-28 22:14:44 -0500

    fixed some of the issue with the centerFrame

--- GUI.py
+++ GUI.py
@@ -316,7 +316,7 @@ class GUI(object):
         self.displayNameCheckbutton.grid(row=1, column=1)
 
         self.displayName.set("<name>")
-        self.displayNameEntry = ttk.Entry(cf, textvariable=self.displayName, state=off)
+        self.displayNameEntry = ttk.Entry(cf, textvariable=self.displayName, state=off, width=30) # setting the width to 30 here widens the rest of them
         self.displayNameEntry.grid(row=2, column=0, sticky="ew", padx=(indent,0))
 
 
--- MissionCompiler.py
+++ MissionCompiler.py
@@ -18,15 +18,23 @@ class MissionCompiler(object):
 
         # step through each Checkbutton's corresponding EntryState
         #   if selected, saved the data it guards
+
+        # mission display name
         if self.esmb.displayNameEntryState:
-            print("Found display name: " + self.esmb.displayName.get())
+            print("\tFound display name: " + self.esmb.displayName.get())
             self.mission.components.missionDisplayName = self.esmb.displayName.get()
+
+        # description
         if self.esmb.descriptionEntryState:
-            print("Found description: " + self.esmb.description.get())
+            print("\tFound description: " + self.esmb.description.get())
             self.mission.components.description = self.esmb.description.get()
+
+        # is blocking
         if self.esmb.isBlockedEntryState:
-            print("Found block: " + self.esmb.isBlockedMessage.get())
+            print("\tFound block: " + self.esmb.isBlockedMessage.get())
             self.mission.components.blocked = self.esmb.isBlockedMessage.get()
+
+        # deadline
 
 
         self.mission.parseMission()
