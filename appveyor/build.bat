4cd863e2658938f05b319b29d0f90b2c69eca4c9
shitwolfymakes <bhawk11@gmail.com>
2019-06-15 12:26:41 -0600

    Patched

--- appveyor/build.bat
+++ appveyor/build.bat
@@ -2,16 +2,16 @@ REM Setup
 C:\Python37-x64\python -m pip install nuitka pyinstaller
 
 REM Nuitka Compilation
-C:\Python37-x64\python -m nuitka --assume-yes-for-downloads --standalone --show-progress --show-scons --plugin-enable=tk-inter ESMB.py
+C:\Python37-x64\python -m nuitka --assume-yes-for-downloads --standalone --show-progress --show-scons --plugin-enable=tk-inter --windows-disable-console --windows-icon=icon.ico ESMB.py
 mv ESMB.dist ESMB
 7z a -tzip -mx9 -y ESMB-win64-nuitka.zip .\ESMB\
 rd /S /Q ESMB
 
 REM PyInstaller
-C:\Python37-x64\Scripts\pyinstaller -D -y -w ESMB.py
+C:\Python37-x64\Scripts\pyinstaller -D -y -w -i icon.ico ESMB.py
 7z a -tzip -mx9 -y ESMB-win64-pyinstaller.zip .\dist\ESMB
 rd /S /Q dist
 
-C:\Python37-x64\Scripts\pyinstaller -F -y -w ESMB.py
+C:\Python37-x64\Scripts\pyinstaller -F -y -w -i icon.ico ESMB.py
 cp dist\ESMB ESMB-win64-pyinstaller.exe
 rd /S /Q dist
