186a02b9f571cb13a005382a58dbb7beb7378d9c
M*C*O <mcofficer@gmx.de>
2019-06-28 18:43:40 +0200

    Add license

--- appveyor/ttkthemes_nuitka_plugin.py
+++ appveyor/ttkthemes_nuitka_plugin.py
@@ -1,3 +1,17 @@
+''' ttkthemes_nuitka_plugin.py
+# Copyright (c) 2019 by MCOfficer
+#
+# Endless Sky Mission Builder is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# Endless Sky Mission Builder is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+This file contains a plug-in that fixes standalone nuitka builds with the ttkthemes module.
+'''
+
 import os
 from logging import info, warning
 from shutil import copytree
