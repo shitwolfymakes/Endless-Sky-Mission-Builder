020f5591e487caba93e9b60428c4f4bd21bc0488
shitwolfymakes <bhawk11@gmail.com>
2019-09-14 23:03:30 -0500

    Fixed imports

--- src/widgets/__init__.py
+++ src/widgets/__init__.py
@@ -5,14 +5,14 @@ widgets
 Provides custom widgets to encapsulate complex, common parts of ESMB's gui
 """
 
-from .AggregatedTriggerFrame import AggregatedTriggerFrame
 from .AggregatedTriggerFrame import TriggerFrame
 from .AggregatedTriggerFrame import TriggerWindow
 from .AggregatedTriggerFrame import AggregatedLogFrame
 from .AggregatedTriggerFrame import LogFrame
 from .AggregatedTriggerFrame import LogWindow
-from src.widgets.AggregatedTriggerConditionFrame import AggregatedTriggerConditionFrame
 
+from .AggregatedTriggerFrame import AggregatedTriggerFrame
+from .AggregatedTriggerConditionFrame import AggregatedTriggerConditionFrame
 from .ComponentMandOptFrame import ComponentMandOptFrame
 from .ComboComponentFrame import ComboComponentFrame
 from .PopupWindow import PopupWindow
