da590f249af61a4a37a3fbe18d0e1cb4a499d21a
shitwolfymakes <bhawk11@gmail.com>
2019-09-14 23:15:15 -0500

    fixed imports

--- src/widgets/__init__.py
+++ src/widgets/__init__.py
@@ -7,14 +7,13 @@ Provides custom widgets to encapsulate complex, common parts of ESMB's gui
 
 from .AggregatedTriggerFrame import TriggerFrame
 from .AggregatedTriggerFrame import TriggerWindow
-from src.widgets.AggregatedLogFrame import AggregatedLogFrame
-from src.widgets.LogFrame import LogFrame
-from src.widgets.LogWindow import LogWindow
 
+from .AggregatedLogFrame import AggregatedLogFrame
 from .AggregatedTriggerFrame import AggregatedTriggerFrame
 from .AggregatedTriggerConditionFrame import AggregatedTriggerConditionFrame
 from .ComboComponentFrame import ComboComponentFrame
 from .ComponentMandOptFrame import ComponentMandOptFrame
+from .LogFrame import LogFrame
 from .LogWindow import LogWindow
 from .PopupWindow import PopupWindow
 from .ScrollingCenterFrame import ScrollingCenterFrame
