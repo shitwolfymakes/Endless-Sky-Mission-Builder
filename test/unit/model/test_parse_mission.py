de5a12f5e15df4f377b6f5f23210b4fa5df31ac4
shitwolfymakes <bhawk11@gmail.com>
2019-09-24 18:31:58 -0500

    rearranged code

--- test/unit/model/test_parse_mission.py
+++ test/unit/model/test_parse_mission.py
@@ -5,6 +5,53 @@ from src.model.Mission import Mission
 class ParseMissionTestCase(unittest.TestCase):
     """Tests for `Mission.parse_mission`"""
 
+
+    def test_parse_mission(self):
+        true_output = self.add_all_data_lines()
+
+        self.model.parse_mission()
+        self.assertEqual(true_output, self.model.missionLines)
+    #end test_parse_mission
+
+
+    def add_all_data_lines(self):
+        return ['mission "Test Mission 0"\n',
+                '\tname `test`\n',
+                '\tdescription `A test mission`\n',
+                '\tblocked "Oh piss off!"\n',
+                '\tdeadline 2 1\n',
+                '\tcargo "food" 5 2 0.1\n',
+                '\tpassengers 5 5 0.2\n',
+                '\tillegal 50 `Soviet citizens need no food comrade`\n',
+                '\tstealth\n',
+                '\tinvisible\n',
+                '\tpriority\n',
+                '\tjob\n',
+                '\trepeat 5\n',
+                "\tclearance `You're on the list`\n",
+                '\tinfiltrating\n',
+                '\twaypoint "Sol"\n',
+                '\tstopover "Delve"\n',
+                '\tsource "Sol"\n',
+                '\tdestination "Delve"\n',
+                '\ton accept\n',
+                '\t\toutfit "test outfit" 1\n',
+                '\t\trequire "Jump Drive" 1\n',
+                '\ton offer\n',
+                '\t\tdialog `It is Wednesday my dudes`\n',
+                '\t\toutfit "Skylance V" 5\n',
+                '\t\trequire Hyperdrive 1\n',
+                '\t\tpayment 1500 0.2\n',
+                '\t\t"yo mama" += 20\n',
+                '\t\t"no u" ++\n',
+                '\t\tclear "the drugs"\n',
+                '\t\tevent "blaze it" 420 4200\n',
+                '\t\tfail "the mission"\n',
+                "\t\tlog `my mama ain't a ho`\n",
+                '\t\tlog "People" "Yo mama" `is a ho`\n']
+    #end add_all_data_lines
+
+
     def setUp(self):
         self.model = Mission("Test Mission 0")
         self.model.components.missionDisplayName = "test"
@@ -58,52 +105,6 @@ class ParseMissionTestCase(unittest.TestCase):
         trigger.add_log()
         trigger.logs[1].set(3, ["People", "Yo mama", "is a ho"])
     #end setUp
-
-
-    def test_parse_mission(self):
-        true_output = self.add_all_data_lines()
-
-        self.model.parse_mission()
-        self.assertEqual(true_output, self.model.missionLines)
-    #end test_parse_mission
-
-
-    def add_all_data_lines(self):
-        return ['mission "Test Mission 0"\n',
-                '\tname `test`\n',
-                '\tdescription `A test mission`\n',
-                '\tblocked "Oh piss off!"\n',
-                '\tdeadline 2 1\n',
-                '\tcargo "food" 5 2 0.1\n',
-                '\tpassengers 5 5 0.2\n',
-                '\tillegal 50 `Soviet citizens need no food comrade`\n',
-                '\tstealth\n',
-                '\tinvisible\n',
-                '\tpriority\n',
-                '\tjob\n',
-                '\trepeat 5\n',
-                "\tclearance `You're on the list`\n",
-                '\tinfiltrating\n',
-                '\twaypoint "Sol"\n',
-                '\tstopover "Delve"\n',
-                '\tsource "Sol"\n',
-                '\tdestination "Delve"\n',
-                '\ton accept\n',
-                '\t\toutfit "test outfit" 1\n',
-                '\t\trequire "Jump Drive" 1\n',
-                '\ton offer\n',
-                '\t\tdialog `It is Wednesday my dudes`\n',
-                '\t\toutfit "Skylance V" 5\n',
-                '\t\trequire Hyperdrive 1\n',
-                '\t\tpayment 1500 0.2\n',
-                '\t\t"yo mama" += 20\n',
-                '\t\t"no u" ++\n',
-                '\t\tclear "the drugs"\n',
-                '\t\tevent "blaze it" 420 4200\n',
-                '\t\tfail "the mission"\n',
-                "\t\tlog `my mama ain't a ho`\n",
-                '\t\tlog "People" "Yo mama" `is a ho`\n']
-    #end add_all_data_lines
 #end class ParseMissionTestCase
 
 
