7d060d2c7d31b882f14266708028d3dea670472f
shitwolfymakes <bhawk11@gmail.com>
2019-09-24 19:22:04 -0500

    Added test

--- test/unit/model/test_mission_parser.py
+++ test/unit/model/test_mission_parser.py
@@ -15,6 +15,7 @@ class MissionParserTestCase(unittest.TestCase):
     #end test_add_line
 
 
+    ### mission display name
     def test_has_mission_display_name_true(self):
         test_model = self.get_empty_test_model()
         test_model.components.missionDisplayName = "Welcome To The Jungle"
@@ -25,6 +26,15 @@ class MissionParserTestCase(unittest.TestCase):
     def test_has_mission_display_name_false(self):
         test_model = self.get_empty_test_model()
         self.assertFalse(test_model._has_mission_display_name())
+    #end test_has_mission_display_name_true
+
+
+    def test_parse_mission_display_name(self):
+        true_output = "\tname `Welcome To The Jungle`\n"
+        test_model = self.get_empty_test_model()
+        test_model.components.missionDisplayName = "Welcome To The Jungle"
+        test_model._parse_mission_display_name()
+        self.assertEqual(true_output, test_model.lines[0])
     #end test_has_mission_display_name_true
 
 
