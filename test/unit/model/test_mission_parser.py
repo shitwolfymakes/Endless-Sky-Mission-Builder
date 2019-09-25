31041fe3b31ac857afef8ab1e43364ac6eceb8f9
shitwolfymakes <bhawk11@gmail.com>
2019-09-24 19:28:36 -0500

    Added tests for description

--- test/unit/model/test_mission_parser.py
+++ test/unit/model/test_mission_parser.py
@@ -18,7 +18,7 @@ class MissionParserTestCase(unittest.TestCase):
     ### mission display name
     def test_has_mission_display_name_true(self):
         test_model = self.get_empty_test_model()
-        test_model.components.missionDisplayName = "Welcome To The Jungle"
+        test_model.components.missionDisplayName = 'Welcome To The Jungle'
         self.assertTrue(test_model._has_mission_display_name())
     #end test_has_mission_display_name_true
 
@@ -26,16 +26,39 @@ class MissionParserTestCase(unittest.TestCase):
     def test_has_mission_display_name_false(self):
         test_model = self.get_empty_test_model()
         self.assertFalse(test_model._has_mission_display_name())
-    #end test_has_mission_display_name_true
+    #end test_has_mission_display_name_false
 
 
     def test_parse_mission_display_name(self):
-        true_output = "\tname `Welcome To The Jungle`\n"
+        true_output = '\tname `Welcome To The Jungle`\n'
         test_model = self.get_empty_test_model()
-        test_model.components.missionDisplayName = "Welcome To The Jungle"
+        test_model.components.missionDisplayName = 'Welcome To The Jungle'
         test_model._parse_mission_display_name()
         self.assertEqual(true_output, test_model.lines[0])
-    #end test_has_mission_display_name_true
+    #end test_parse_mission_display_name
+
+
+    ### description
+    def test_has_description_true(self):
+        test_model = self.get_empty_test_model()
+        test_model.components.description = 'A test mission'
+        self.assertTrue(test_model._has_description())
+    # end test_has_description_true
+
+
+    def test_has_description_false(self):
+        test_model = self.get_empty_test_model()
+        self.assertFalse(test_model._has_description())
+    # end test_has_description_false
+
+
+    def test_parse_description(self):
+        true_output = '\tdescription `A test mission`\n'
+        test_model = self.get_empty_test_model()
+        test_model.components.description = 'A test mission'
+        test_model._parse_description()
+        self.assertEqual(true_output, test_model.lines[0])
+    #end test_parse_description
 
 
     @staticmethod
