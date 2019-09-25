510e356e4fecb19f239fe60d5aba1ded880a6418
shitwolfymakes <bhawk11@gmail.com>
2019-09-24 19:09:26 -0500

    Added utility method

--- test/unit/model/test_mission_parser.py
+++ test/unit/model/test_mission_parser.py
@@ -8,10 +8,16 @@ class MissionParserTestCase(unittest.TestCase):
     def test_add_line(self):
         true_output = ["Harambe died for you\n"]
 
-        test_model = model.MissionParser(model.Mission("Testing"))
+        test_model = self.get_empty_test_model()
         test_model._add_line("Harambe died for you")
 
         self.assertEqual(true_output, test_model.lines)
     #end test_add_line
+
+
+    @staticmethod
+    def get_empty_test_model():
+        return model.MissionParser(model.Mission("Testing"))
+    #end get_empty_test_model
 
 #end class MissionParserTestCase
