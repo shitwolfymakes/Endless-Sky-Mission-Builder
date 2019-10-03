a9f71ad8e7d4601edda61f540252e2b29b14b9b4
shitwolfymakes <bhawk11@gmail.com>
2019-10-03 05:15:08 -0500

    ESMB now loads tooltips into a global config

--- src/ESMB.py
+++ src/ESMB.py
@@ -18,35 +18,45 @@ Endless Sky is made by Michael Zahniser.
 My Github: https://github.com/shitwolfymakes
 Endless Sky Github: https://github.com/endless-sky/endless-sky
 """
+import sys
+import logging
 
-from src.gui.GUI import *
+from src.gui.GUI import GUI
+import src.utils as utils
+import src.config as config
 
 
 #TODO: Have ESMB hold the missionList instead of GUI
 class ESMB(object):
     """The application object"""
-
     def __init__(self):
-        logger_setup()
+        self.logger_setup()
         logging.debug("Starting ESMB...")
 
         debug_mode = False
         if "debug=True" in sys.argv:
             debug_mode = True
 
+        self.load_tooltips()
         self.gui = GUI(debug_mode)
     #end init
 
+    @staticmethod
+    def logger_setup():
+        # For some reason this for loop is required to get the outputting to a file working
+        # See here: https://stackoverflow.com/a/49202811
+        for handler in logging.root.handlers[:]:
+            logging.root.removeHandler(handler)
+        logging.basicConfig(filename='log.txt', filemode='w', level=logging.DEBUG)
+    # end logger setup
+
+    @staticmethod
+    def load_tooltips():
+        config.tooltips_dict = utils.load_tooltips()
+        print(config.tooltips_dict)
+        logging.debug("\tTooltips loaded!")
+    # end load_tooltips
 #end class ESMB
-
-
-def logger_setup():
-    # For some reason this for loop is required to get the outputting to a file working
-    # See here: https://stackoverflow.com/a/49202811
-    for handler in logging.root.handlers[:]:
-        logging.root.removeHandler(handler)
-    logging.basicConfig(filename='log.txt', filemode='w', level=logging.DEBUG)
-#end logger setup
 
 
 def main():
