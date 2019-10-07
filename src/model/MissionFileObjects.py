ecdef300f4e0e7432f9eed7b504e344241500589
shitwolfymakes <bhawk11@gmail.com>
2019-10-07 16:07:40 -0500

    initial commit

---
+++ src/model/MissionFileObjects.py
@@ -0,0 +1,41 @@
+""" MissionFileObjects.py
+# Copyright (c) 2019 by Andrew Sneed
+#
+# Endless Sky Mission Builder is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# Endless Sky Mission Builder is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+"""
+
+
+class MissionFileObjects(object):
+    """THis class provides a singleton that stores a list of FileObjects"""
+    class __MissionFileObjects:
+        def __init__(self):
+            self.objects_list = []
+        #end init
+
+
+        def __str__(self):
+            return repr(self) + str(self.objects_list)
+        #end str
+    #end class __MissionFileObjects
+
+    instance = None
+
+    def __init__(self):
+        if not MissionFileObjects.instance:
+            MissionFileObjects.instance = MissionFileObjects.__MissionFileObjects()
+        else:
+            pass
+        #end if/else
+    #end init
+
+
+    def __getattr__(self, name):
+        return getattr(self.instance, name)
+    #end getattr
+#end class MissionFileObjects
