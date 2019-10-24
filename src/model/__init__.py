895ccd52cf6b0eeb198f3b29fd7d6ed1f54d7524
shitwolfymakes <bhawk11@gmail.com>
2019-10-24 00:34:08 -0500

    Added TODO

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -7,7 +7,7 @@ Provides
     2. Ways to read in and write out mission data to files
     2. Take what is entered into the gui and store it into the model
 """
-
+#TODO: restructure this package to have a subpackage for mission, event, and phrase
 from .FileItem import FileItem
 from .Mission import Mission
 from .MissionCompiler import MissionCompiler
