ab2750ee6bde8eb66fa4b1f9598be406351776d3
shitwolfymakes <bhawk11@gmail.com>
2019-10-29 20:21:56 -0500

    possible fix for tooltips file win/Nix

--- src/utils/loadtooltips.py
+++ src/utils/loadtooltips.py
@@ -12,15 +12,13 @@
 
 import logging
 import json
-import os
 
 
 def load_tooltips():
     logging.debug("\tLoading tooltips...")
-    json_path = os.path.join(os.path.dirname(os.path.realpath("ESMB.py")), "../data/tooltips.json")
-    print(json_path)
-    with open(json_path) as data_file:
-        logging.debug("\t\tTooltips JSON file opened...")
+    #print(os.getcwd())
+    with open("data/tooltips.json") as data_file:
+        logging.debug("\tTooltips JSON file opened...")
         data = json.load(data_file)
     #end with open
     return data
