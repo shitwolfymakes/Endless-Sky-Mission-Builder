563bed593500db09c7b889bda794c2f8197c162b
shitwolfymakes <bhawk11@gmail.com>
2019-11-07 12:26:22 -0600

    attempting to get CI working

--- appveyor/build.sh
+++ appveyor/build.sh
@@ -4,17 +4,20 @@ sudo apt-get update && sudo apt-get -y install python3-pip python3-tk chrpath
 pip3 install nuitka PyInstaller
 pip3 install -r requirements.txt
 
+# move into the folder containing ESMB.py
+cd src
+
 # Nuitka Compilation
-python3 -m nuitka --assume-yes-for-downloads --standalone --show-progress --show-scons --user-plugin=appveyor/ttkthemes_nuitka_plugin.py src/ESMB.py
+python3 -m nuitka --assume-yes-for-downloads --standalone --show-progress --show-scons --user-plugin=appveyor/ttkthemes_nuitka_plugin.py ESMB.py
 mv ESMB.dist ESMB
 tar -czvf ESMB-ubuntu-amd64-nuitka.tar.gz ESMB/
 rm -rf ESMB
 
 # PyInstaller
-pyinstaller -D -y -w --hidden-import ttkthemes src/ESMB.py
+pyinstaller -D -y -w --hidden-import ttkthemes ESMB.py
 tar -czvf ESMB-ubuntu-amd64-pyinstaller.tar.gz dist/ESMB/
 rm -rf dist
 
-pyinstaller -F -y -w --hidden-import ttkthemes src/ESMB.py
+pyinstaller -F -y -w --hidden-import ttkthemes ESMB.py
 cp dist/ESMB ESMB-ubuntu-amd64-pyinstaller
 rm -rf dist
