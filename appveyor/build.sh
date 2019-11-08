38c8594a2d7800827934bf75038158138bb13fc5
shitwolfymakes <bhawk11@gmail.com>
2019-11-08 13:27:04 -0600

    attempting to get CI working

--- appveyor/build.sh
+++ appveyor/build.sh
@@ -1,7 +1,7 @@
 # Setup
 # chrpath can be required for nuitka's scons command, to adjust shared library paths
 sudo apt-get update && sudo apt-get -y install python3-pip python3-tk chrpath
-pip3 install nuitka PyInstaller
+pip3 install nuitka #PyInstaller
 pip3 install -r requirements.txt
 
 # move into the folder containing ESMB.py
@@ -14,10 +14,10 @@ tar -czvf ESMB-ubuntu-amd64-nuitka.tar.gz ESMB/
 rm -rf ESMB
 
 # PyInstaller
-python3 -m pyinstaller -D -y -w --hidden-import ttkthemes ESMB.py
-tar -czvf ESMB-ubuntu-amd64-pyinstaller.tar.gz dist/ESMB/
-rm -rf dist
+#python3 -m pyinstaller -D -y -w --hidden-import ttkthemes ESMB.py
+#tar -czvf ESMB-ubuntu-amd64-pyinstaller.tar.gz dist/ESMB/
+#rm -rf dist
 
-python3 -m pyinstaller -F -y -w --hidden-import ttkthemes ESMB.py
-cp dist/ESMB ESMB-ubuntu-amd64-pyinstaller
-rm -rf dist
+#python3 -m pyinstaller -F -y -w --hidden-import ttkthemes ESMB.py
+#cp dist/ESMB ESMB-ubuntu-amd64-pyinstaller
+#rm -rf dist
