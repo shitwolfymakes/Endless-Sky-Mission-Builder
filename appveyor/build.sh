2f52f2b71fde65c889757fb109898ae2afa2e670
shitwolfymakes <bhawk11@gmail.com>
2019-11-08 15:44:34 -0600

    attempting to get CI working

--- appveyor/build.sh
+++ appveyor/build.sh
@@ -8,7 +8,7 @@ pip3 install -r requirements.txt
 #cd src
 
 # Nuitka Compilation
-python3 -m nuitka --include-package=src --assume-yes-for-downloads --standalone --show-progress --show-scons --user-plugin=./appveyor/ttkthemes_nuitka_plugin.py src/ESMB.py
+python3 -m nuitka --include-package=src --follow-imports --assume-yes-for-downloads --standalone --show-progress --show-scons --user-plugin=./appveyor/ttkthemes_nuitka_plugin.py src/ESMB.py
 ls
 mv ESMB.dist ESMB
 tar -czvf ESMB-ubuntu-amd64-nuitka.tar.gz ESMB/
