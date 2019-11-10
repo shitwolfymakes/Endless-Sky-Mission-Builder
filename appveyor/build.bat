45eb1beb91e173dd4ee6d432095ca2dd3df3f4ad
M*C*O <mcofficer@gmx.de>
2019-11-10 15:11:34 +0100

    adjust windows build script

--- appveyor/build.bat
+++ appveyor/build.bat
@@ -6,31 +6,25 @@ REM Setup required Packages
 python -m pip install nuitka PyInstaller
 python -m pip install -r requirements.txt
 
-REM Switch to our source directory
-PUSHD .\src
-REM Now that we are in ".\src", paths relative to the top level have to be prefixed with "..\", e.g. "..\icon.ico"
-
 REM Nuitka Compilation
 REM The two plugin-related arguments are responsible for copying all required tkinter- and ttkthemes-files to the final distribution folder
-python -m nuitka --assume-yes-for-downloads --standalone --show-progress --show-scons --user-plugin=..\appveyor\ttkthemes_nuitka_plugin.py --plugin-enable=tk-inter --windows-disable-console --windows-icon=..\icon.ico .\ESMB.py
+REM python -m nuitka --assume-yes-for-downloads --standalone --show-progress --show-scons --user-plugin=.\appveyor\ttkthemes_nuitka_plugin.py --plugin-enable=tk-inter --windows-disable-console --windows-icon=.\icon.ico .\src\ESMB.py
 MOVE .\ESMB.dist .\ESMB
 REM Archive with maximum zip compression
-7z a -tzip -mx9 -y ..\ESMB-win64-nuitka.zip .\ESMB\
+7z a -tzip -mx9 -y .\ESMB-win64-nuitka.zip .\ESMB\
 REM Cleanup
 RD /S /Q ESMB
 
 REM Build a directory distribution ("-D") with PyInstaller
-pyinstaller -D --noconfirm --noconsole --icon ..\icon.ico --hidden-import ttkthemes .\ESMB.py
+REM We have to add the data folder here since it won't be added automatically. See utils/loadtooltips.py for an example of how to access it
+pyinstaller -D --noconfirm --noconsole --icon .\icon.ico --hidden-import ttkthemes --add-data ".\src\data;data" .\src\ESMB.py
 REM Archive with maximum zip compression
-7z a -tzip -mx9 -y ..\ESMB-win64-pyinstaller.zip .\dist\ESMB
+7z a -tzip -mx9 -y .\ESMB-win64-pyinstaller.zip .\dist\ESMB
 REM Cleanup
 RD /S /Q dist
 
 REM Build a OneFile executable ("-F") with PyInstaller
-pyinstaller -F --noconfirm --noconsole --icon ..\icon.ico --hidden-import ttkthemes .\ESMB.py
-COPY .\dist\ESMB.exe ..\ESMB-win64-pyinstaller.exe
+pyinstaller -F --noconfirm --noconsole --icon .\icon.ico --hidden-import ttkthemes --add-data ".\src\data;data" .\src\ESMB.py
+COPY .\dist\ESMB.exe .\ESMB-win64-pyinstaller.exe
 REM Cleanup
 RD /S /Q dist
-
-REM Return to top level, just to be sure
-POPD
