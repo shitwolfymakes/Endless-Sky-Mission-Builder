9892ee9f19fd8b3737525653297357788124a9d1
M*C*O <mcofficer@gmx.de>
2019-11-10 19:33:36 +0100

    call pip from python3.7, not as pip3

--- appveyor/build.sh
+++ appveyor/build.sh
@@ -2,8 +2,8 @@ sudo apt-get update && sudo apt-get -y install python3-pip python3-tk python3.7 
 python3.7 --version # Make sure we are using the right version - it HAS to be 3.7+, otherwise PyInstaller binaries will have import errors
 
 # Setup required python packages
-pip3 install PyInstaller
-pip3 install -r requirements.txt
+python3.7 -m pip install PyInstaller
+python3.7 -m pip install -r requirements.txt
 
 # Build a directory distribution ("-D") with PyInstaller
 # We have to add the data folder here since it won't be added automatically. See utils/loadtooltips.py for an example of how to access it
