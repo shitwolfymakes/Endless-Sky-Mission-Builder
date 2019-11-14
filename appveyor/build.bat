5951047846a56e4112595629edf6400ad1a96268
M*C*O <mcofficer@gmx.de>
2019-11-14 14:10:35 +0100

    Fix appveyor deployment (#17)
    * reenable windows builds
    
    * Rewrite windows build script to be more verbose & elegant
    
    - Consistently use .\ for folders and files
    - More comments
    - Prefer verbose parameter names unless explicitly explained
    - Centralize paths to python installation
    - Prefer explicit windows command names in over shorthand aliases
    
    * let nuitka choose its compiler freely
    
    * compatibility patches in the code
    
    * adjust windows build script
    
    * adapt for linux, remove nuitka compilation
    
    * fixup PyInstaller package calls
    
    * ubuntu: install python3.7 explicitly
    
    * call pip from python3.7, not as pip3
    
    * remove python version check, restructure comments

--- appveyor/build.bat
+++ appveyor/build.bat
@@ -1,20 +1,22 @@
-REM Setup
-C:\Python37-x64\python -m pip install nuitka PyInstaller
-C:\Python37-x64\python -m pip install -r requirements.txt
+REM Add our desired Python installation the *beginning* of the PATH, so the "python" command refers to this executable.
+REM It HAS to be 3.7+, otherwise PyInstaller binaries will have import errors for some reason.
+REM Also add its "Scripts" folder, which will contain the PyInstaller executable once installed.
+set PATH=C:\Python37-x64;C:\Python37-x64\Scripts;%PATH%
 
-PUSHD ".\src"
+REM Setup required Packages
+python -m pip install PyInstaller
+python -m pip install -r requirements.txt
 
-REM Nuitka Compilation
-C:\Python37-x64\python -m nuitka --assume-yes-for-downloads --standalone --show-progress --show-scons --user-plugin=appveyor/ttkthemes_nuitka_plugin.py --plugin-enable=tk-inter --windows-disable-console --windows-icon=icon.ico ESMB.py
-mv ESMB.dist ESMB
-7z a -tzip -mx9 -y ESMB-win64-nuitka.zip .\ESMB\
-rd /S /Q ESMB
+REM Build a directory distribution ("-D") with PyInstaller
+REM We have to add the data folder here since it won't be added automatically. See utils/loadtooltips.py for an example of how to access it
+pyinstaller -D --noconfirm --noconsole --icon .\icon.ico --hidden-import ttkthemes --add-data ".\src\data;data" .\src\ESMB.py
+REM Archive with maximum zip compression
+7z a -tzip -mx9 -y .\ESMB-win64-pyinstaller.zip .\dist\ESMB
+REM Cleanup
+RD /S /Q dist
 
-REM PyInstaller
-C:\Python37-x64\Scripts\pyinstaller -D -y -w -i icon.ico --hidden-import ttkthemes ESMB.py
-7z a -tzip -mx9 -y ESMB-win64-pyinstaller.zip .\dist\ESMB
-rd /S /Q dist
-
-C:\Python37-x64\Scripts\pyinstaller -F -y -w -i icon.ico --hidden-import ttkthemes ESMB.py
-cp dist\ESMB ESMB-win64-pyinstaller.exe
-rd /S /Q dist
+REM Build a OneFile executable ("-F") with PyInstaller
+pyinstaller -F --noconfirm --noconsole --icon .\icon.ico --hidden-import ttkthemes --add-data ".\src\data;data" .\src\ESMB.py
+COPY .\dist\ESMB.exe .\ESMB-win64-pyinstaller.exe
+REM Cleanup
+RD /S /Q dist
--- appveyor/build.sh
+++ appveyor/build.sh
@@ -1,24 +1,22 @@
-# Setup
-# chrpath can be required for nuitka's scons command, to adjust shared library paths
-sudo apt-get update && sudo apt-get -y install python3-pip python3-tk chrpath
-pip3 install nuitka #PyInstaller
-pip3 install -r requirements.txt
+# Install pip, python3-tk (tkinter package), python3.7 and python3.7-dev (to have libraries available)
+# Python HAS to be version 3.7+, otherwise PyInstaller binaries will have import errors for some reason
+sudo apt-get update && sudo apt-get -y install python3-pip python3-tk python3.7 python3.7-dev
 
-# move into the folder containing ESMB.py
-#cd src
+# Setup required python packages
+python3.7 -m pip install PyInstaller
+python3.7 -m pip install -r requirements.txt
 
-# Nuitka Compilation
-python3 -m nuitka --include-package=src --follow-imports --assume-yes-for-downloads --standalone --show-progress --show-scons --user-plugin=./appveyor/ttkthemes_nuitka_plugin.py src/ESMB.py
-ls
-mv ESMB.dist ESMB
-tar -czvf ESMB-ubuntu-amd64-nuitka.tar.gz ESMB/
-rm -rf ESMB
+# Build a directory distribution ("-D") with PyInstaller
+# We have to add the data folder here since it won't be added automatically. See utils/loadtooltips.py for an example of how to access it
+python3.7 -m PyInstaller -D --noconfirm --hidden-import ttkthemes --add-data "src/data:data" src/ESMB.py
+# Archive with maximum zip compression
+env GZIP=-9 tar -czf ./ESMB-ubuntu-amd64-pyinstaller.tar.gz dist/ESMB/
+# Cleanup
+rm -rf dist/
 
-# PyInstaller
-#python3 -m pyinstaller -D -y -w --hidden-import ttkthemes ESMB.py
-#tar -czvf ESMB-ubuntu-amd64-pyinstaller.tar.gz dist/ESMB/
-#rm -rf dist
-
-#python3 -m pyinstaller -F -y -w --hidden-import ttkthemes ESMB.py
-#cp dist/ESMB ESMB-ubuntu-amd64-pyinstaller
-#rm -rf dist
+# Build a OneFile executable ("-F") with PyInstaller
+# We have to add the data folder here since it won't be added automatically. See utils/loadtooltips.py for an example of how to access it
+python3.7 -m PyInstaller -F --noconfirm --hidden-import ttkthemes --add-data "src/data:data" src/ESMB.py
+cp dist/ESMB ESMB-ubuntu-amd64-pyinstaller
+# Cleanup
+rm -rf dist/
