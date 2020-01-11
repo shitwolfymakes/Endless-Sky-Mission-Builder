c7a9ead20e344cb28babb56ecbbfb2891a0a3409
shitwolfymakes <bhawk11@gmail.com>
2020-01-11 17:51:06 -0600

    added FileNPCItemParser.py

---
+++ src/model/file_data_parsers/FileNPCItemParser.py
@@ -0,0 +1,23 @@
+""" FileNPCItemParser.py
+# Copyright (c) 2020 by Andrew Sneed
+#
+# Endless Sky Mission Builder is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# Endless Sky Mission Builder is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+"""
+from model.file_data_parsers import FileItemParser
+
+
+class FileNPCItemParser(FileItemParser):
+    def __init__(self):
+        pass
+    #end init
+
+    def run(self):
+        pass
+    #end run
+#end FileNPCItemParser
