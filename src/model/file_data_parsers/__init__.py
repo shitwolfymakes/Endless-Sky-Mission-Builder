4ac3f695e773395013a26520adccd051ad59a9a0
shitwolfymakes <bhawk11@gmail.com>
2020-01-11 17:51:22 -0600

    updated imports

--- src/model/file_data_parsers/__init__.py
+++ src/model/file_data_parsers/__init__.py
@@ -9,5 +9,6 @@ This package contains parsers for ES mission files
 from .FileItemParser import FileItemParser      # superclass goes first
 from .FileEventItemParser import FileEventItemParser
 from .FileMissionItemParser import FileMissionItemParser
+from .FileNPCItemParser import FileNPCItemParser
 from .FilePhraseItemParser import FilePhraseItemParser
 from .MissionFileParser import MissionFileParser
