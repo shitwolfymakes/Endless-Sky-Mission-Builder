92b7f87674ad418e2565ff32ecff73fe345568f9
shitwolfymakes <bhawk11@gmail.com>
2020-01-11 16:08:59 -0600

    Added FilePhraseItemParser.py

---
+++ src/model/file_data_parsers/FilePhraseItemParser.py
@@ -0,0 +1,12 @@
+from model.file_data_parsers import FileItemParser
+
+
+class FilePhraseItemParser(FileItemParser):
+    def __init__(self):
+        pass
+    #end init
+
+    def run(self):
+        pass
+    #end run
+#end FilePhraseItemParser
