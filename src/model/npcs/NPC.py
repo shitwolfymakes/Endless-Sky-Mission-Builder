97530a78bd9cfaa3a34a78958f82b32847933379
shitwolfymakes <bhawk11@gmail.com>
2020-01-11 17:49:32 -0600

    added NPC.py

---
+++ src/model/npcs/NPC.py
@@ -0,0 +1,32 @@
+""" NPC.py
+# Copyright (c) 2019 by Andrew Sneed
+#
+# Endless Sky Mission Builder is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# Endless Sky Mission Builder is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+"""
+import logging
+
+import src.model as model
+
+
+class NPC(model.FileItem):
+    #TODO: implement this
+    def __init__(self, name):
+        logging.debug("Building npc %s", name)
+
+        super().__init__("npc")
+        self.name = name
+
+        self.parse()
+    #end init
+
+
+    def parse(self):
+        pass
+    #end parse
+#end class Event
---
+++ src/model/npcs/__init__.py
@@ -0,0 +1,8 @@
+"""
+npcs
+====
+
+Provides all the custom objects needed to handle npcs
+"""
+
+from .NPC import NPC
