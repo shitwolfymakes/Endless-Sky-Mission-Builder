7c5efb7b9803ccd5b94d6bd8327f993b88a5516f
shitwolfymakes <bhawk11@gmail.com>
2020-01-19 10:49:29 -0600

    added init method

--- src/widgets/npc/NPCWindow.py
+++ src/widgets/npc/NPCWindow.py
@@ -10,7 +10,46 @@
 # PARTICULAR PURPOSE. See the GNU General Public License for more details.
 """
 
+import logging
+from tkinter import *
+from tkinter import ttk
 
-class NPCWindow:
-    pass
+import src.widgets as widgets
+from src import config
+
+
+class NPCWindow(Toplevel):
+    """This class creates a custom pop-up window to display and edit the data in an associated NPC object"""
+
+    def __init__(self, master, npc):
+        logging.debug("\tBuilding NPCWindow...")
+        super().__init__(master)
+
+        self.npc = npc
+        config.active_item = npc
+
+        self.title("Edit Trigger")
+        self.configure(bg="#ededed")
+        self.grab_set()  # freezes the app until the user enters or cancels
+
+        outer = ttk.Frame(self)
+        outer.pack(side=TOP)
+
+        self.left_frame = ttk.Frame(outer)
+        self.left_frame.pack(side=LEFT)
+
+        self.right_frame = ttk.Frame(outer)
+        self.right_frame.pack(side=RIGHT, anchor=N)
+
+        self.close_button = ttk.Button(self, text="Ok", command=self._cleanup)
+        self.close_button.pack(side=BOTTOM)
+
+        ### BUILDING LEFT FRAME ###
+        #TODO: implement this
+
+        ### BUILDING RIGHT FRAME###
+        # TODO: implement this
+
+        self._populate_window()
+    #end init
 #end class NPCWindow
