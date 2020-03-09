efc9706ccc19965aee413ac18b0a4679fb22acab
shitwolfymakes <bhawk11@gmail.com>
2020-03-08 23:06:37 -0500

    Added DialogFrame.py

---
+++ src/widgets/DialogFrame.py
@@ -0,0 +1,25 @@
+""" DialogFrame.py
+# Copyright (c) 2019 by Andrew Sneed
+#
+# Endless Sky Mission Builder is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# Endless Sky Mission Builder is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+"""
+
+import logging
+from tkinter import *
+from tkinter import ttk
+
+
+class DialogFrame(ttk.Frame):
+    """This class extends ttk.Frame to create a custom GUI widget"""
+
+    def __init__(self, master, name, populating=False):
+        ttk.Frame.__init__(self, master)
+        pass
+    #end init
+#end class DialogFrame
