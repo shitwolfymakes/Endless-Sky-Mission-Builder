afcbbb61b02abaafd19c4776a1789e4b7fbe70e9
shitwolfymakes <bhawk11@gmail.com>
2020-05-28 18:37:05 -0500

    updated imports

--- src/widgets/fleets/__init__.py
+++ src/widgets/fleets/__init__.py
@@ -4,3 +4,5 @@ fleets
 
 This package contains all custom tkinter widgets that are directly related to custom fleet editing
 """
+
+from .AggregatedCustomFleetFrame import AggregatedCustomFleetFrame
