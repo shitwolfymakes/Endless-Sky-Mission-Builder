39252ddcbd1ac06261a0a68d732661f4602e4468
M*C*O <mcofficer@gmx.de>
2020-05-28 20:27:22 +0200

    maybe fix mac builds?

--- ci/build_macos.sh
+++ ci/build_macos.sh
@@ -9,7 +9,7 @@ python3.7 -m pip install -r requirements.txt
 
 # Build a directory distribution ("-D") with PyInstaller
 # We have to add the data folder here since it won't be added automatically. See utils/loadtooltips.py for an example of how to access it
-python3.7 -m PyInstaller -D --noconfirm --hidden-import ttkthemes --add-data "src/data:data" --windowed src/ESMB.py
+python3.7 -m PyInstaller -D --noconfirm --hidden-import ttkthemes --add-binary='/System/Library/Frameworks/Tk.framework/Tk':'tk' --add-binary='/System/Library/Frameworks/Tcl.framework/Tcl':'tcl' --add-data "src/data:data" --windowed src/ESMB.py
 # Archive with maximum zip compression
 env GZIP=-9 tar -czf ./ESMB-macos10.15-pyinstaller.tar.gz dist/ESMB/
 env GZIP=-9 tar -czf ./ESMB-macos10.15-pyinstaller-app.tar.gz dist/ESMB.app
@@ -18,7 +18,7 @@ rm -rf dist/
 
 # Build a OneFile executable ("-F") with PyInstaller
 # We have to add the data folder here since it won't be added automatically. See utils/loadtooltips.py for an example of how to access it
-python3.7 -m PyInstaller -F --noconfirm --hidden-import ttkthemes --add-data "src/data:data" src/ESMB.py
+python3.7 -m PyInstaller -F --noconfirm --hidden-import ttkthemes --add-binary='/System/Library/Frameworks/Tk.framework/Tk':'tk' --add-binary='/System/Library/Frameworks/Tcl.framework/Tcl':'tcl' --add-data "src/data:data" src/ESMB.py
 cp dist/ESMB ESMB-ubuntu-macos10.15-pyinstaller
 # Cleanup
 rm -rf dist/
