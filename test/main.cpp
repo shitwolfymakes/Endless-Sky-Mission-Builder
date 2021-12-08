b1e80b3a5118927a1d0a8c4bacee1adb4edf3c14
wolfy <wolfy@shitwolfymakes.com>
2021-12-07 22:41:27 -0700

    Initial attempt at adding a google test

---
+++ test/main.cpp
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0-only
+/*
+ * main.cpp
+ *
+ * Copyright (c) 2021, Andrew Sneed <wolfy@shitwolfymakes.com>
+ */
+
+#include "tst_testcasename.h"
+
+#include <gtest/gtest.h>
+
+int main(int argc, char *argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
