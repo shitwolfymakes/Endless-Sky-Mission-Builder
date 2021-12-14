74d020d3e0cd73864e070739b6328347b0ae6ff8
wolfy <wolfy@shitwolfymakes.com>
2021-12-13 21:13:09 -0700

    fixed data source variable

--- test/parsers/filemissionitemparser_tests.cpp
+++ test/parsers/filemissionitemparser_tests.cpp
@@ -275,15 +275,13 @@ TEST_F(FileMissionItemTriggerParserTest, StoreTriggerDialogPhrase) {
     expected.emplace_back("Harambe 1");
 
     int index = 0;
-    std::vector<std::string> lines = minimum_mission_node_lines;
+    std::vector<std::string> lines = minimum_trigger_node_lines;
     lines.emplace_back("\t\tdialog phrase \"Harambe 1\"");
 
     index = parser.parseDialog(&lines, 1, &trigger);
     ASSERT_EQ(index, 1);
     ASSERT_EQ(trigger["dialog_phrase"].dump(), expected.dump());
 }
-
-
 
 //TEST_F(FileMissionItemTriggerParserTest, StoreTriggerConvoWithoutParsing) {}
 
