5f491c571ee9a8281d27ad97da8776a08d2789e9
wolfy <wolfy@shitwolfymakes.com>
2022-01-13 16:20:22 -0700

    made test a bit more robust

--- test/parsers/fileitemparser_tests.cpp
+++ test/parsers/fileitemparser_tests.cpp
@@ -19,20 +19,23 @@ TEST_F(FileItemParserTest, TestCollectNodeLines) {
                                           "\t\t\"<title>\" \"Bossmang\"",
                                           "\t\t\"<title>\" \"Inner\"",
                                           "\t\t\t\"reputation: Inyalowda\" > 100",
-                                          "\t\t\t\"reputation: Beltalowda\" < 100"};
+                                          "\t\t\t\"reputation: Beltalowda\" < 100",
+                                          "\t\t\"<name>\" \"Anderson Dawes\"",
+                                          "\tinfiltrating"};
 
     json expected;
     expected = {"\tsubstitutions",
                 "\t\t\"<title>\" \"Bossmang\"",
                 "\t\t\"<title>\" \"Inner\"",
                 "\t\t\t\"reputation: Inyalowda\" > 100",
-                "\t\t\t\"reputation: Beltalowda\" < 100"};
+                "\t\t\t\"reputation: Beltalowda\" < 100",
+                "\t\t\"<name>\" \"Anderson Dawes\""};
 
     json nodeLines;
     index = FileItemParser::collectNodeLines(&fileLines, index, &nodeLines);
 
 
-    ASSERT_EQ(index, 5);
+    ASSERT_EQ(index, 6);
     ASSERT_EQ(nodeLines["node_lines"], expected);
 }
 
