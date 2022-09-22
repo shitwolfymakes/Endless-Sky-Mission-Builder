165ecd829f88dbbfbe40e8fd4f1ea50bb515af22
wolfy <wolfy@shitwolfymakes.com>
2022-09-21 20:22:50 -0600

    added class FileItemParserUtils

---
+++ src/esjsonlib/common/fileitemparserutils.cpp
@@ -0,0 +1,6 @@
+#include "fileitemparserutils.h"
+
+FileItemParserUtils::FileItemParserUtils()
+{
+
+}
---
+++ src/esjsonlib/common/fileitemparserutils.h
@@ -0,0 +1,11 @@
+#ifndef FILEITEMPARSERUTILS_H
+#define FILEITEMPARSERUTILS_H
+
+
+class FileItemParserUtils
+{
+public:
+    FileItemParserUtils();
+};
+
+#endif // FILEITEMPARSERUTILS_H
