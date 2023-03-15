69dd2f02abaf68b4b690712a45f011eca012e9d8
wolfy <wolfy@shitwolfymakes.com>
2023-03-14 19:46:33 -0600

    added initial tokenizer loop

--- src/esjsonlib/event/fileeventitemparser.cpp
+++ src/esjsonlib/event/fileeventitemparser.cpp
@@ -9,15 +9,26 @@
 
 #include <iostream>
 
+#include <boost/algorithm/string.hpp>
+
+#include "common/fileitemparserutils.h"
+using utils = FileItemParserUtils;
+
 FileEventItemParser::FileEventItemParser(std::vector<std::string> lines) {
     setLines(lines);
 }
 
 json FileEventItemParser::run() {
     std::cout << "Parsing substitutions node to JSON" << std::endl;
+    std::vector<std::string> tokens;
 
-    // TODO: Implement this
-
+    std::vector<std::string> lines = getLines();
+    for (int i = 0; i < static_cast<int>(lines.size()); i++) {
+        // start by tokenizing each line
+        tokens = utils::tokenize(lines.at(i));
+        //std::cout << "LINE: " << tokens.at(0) << std::endl;
+    }
+    //std::cout << "Mission data: " << mission.dump(4) << std::endl;
     return event;
 }
 
