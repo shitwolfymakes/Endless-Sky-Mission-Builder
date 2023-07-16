b529f36cefe24529f7959525d47468f3187aa665
wolfy <wolfy@shitwolfymakes.com>
2023-07-15 23:35:42 -0600

    added FileFleetItemParserUtilStruct harness

---
+++ src/esjsonlib/fleet/filefleetitemparserutil.cpp
@@ -0,0 +1,6 @@
+#include "filefleetitemparserutil.h"
+
+FileFleetItemParserUtil::FileFleetItemParserUtil()
+{
+
+}
---
+++ src/esjsonlib/fleet/filefleetitemparserutil.h
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0-only
+/*
+ * filefleetitemparserutil.h
+ *
+ * Copyright (c) 2023, Andrew Sneed <wolfy@shitwolfymakes.com>
+ */
+
+#ifndef FILEFLEETITEMPARSERUTIL_H
+#define FILEFLEETITEMPARSERUTIL_H
+
+
+struct FileFleetItemParserUtil {
+
+};
+
+#endif // FILEFLEETITEMPARSERUTIL_H
